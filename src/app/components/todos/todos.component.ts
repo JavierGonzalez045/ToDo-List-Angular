c18e1f8c36c9aa18e9fa74d15ca1d29d9fadcba0
xAHRx <alanhoskingrivera@gmail.com>
2021-12-30 09:51:11 -0400

    Alan - Date Alert Added

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -15,19 +15,17 @@ export class TodosComponent implements OnInit {
   inputTodo: string = '';
 
   inputDate?: Date;
-  
+
   constructor() {}
 
   ngOnInit(): void {}
 
   addTodo() {
-    if (this.inputTodo == '') {
+    if (this.inputTodo == '' ) {
       alert("Can't leave the field empty");
     } else if (this.inputTodo.startsWith(' ')) {
       alert("Can't use WhiteSpace");
-    } else if (this.inputTodo.length >= 20) {
-      alert('The character length cannot be greater than 20');
-    } else if (
+    }else if (
       this.todos.filter((data) => data.content === this.inputTodo).length > 0
     ) {
       alert('Cannot insert the same task name!');
@@ -40,7 +38,10 @@ export class TodosComponent implements OnInit {
       });
     }
   }
-
+  blockSpecialChar(e: { keyCode: any; }) {
+    var k = e.keyCode;
+    return ((k > 64 && k < 91) || (k > 96 && k < 123) || k == 8   || (k >= 48 && k))
+    }
   disablecancel(index: number) {
     this.todos.splice(index, 1, {
       content: this.todos[index].content,
